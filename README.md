0b26f126e40ea74cc796fc754450665b9b9f87b7
elmanytas <elmanytas@gmail.com>
2020-04-11 16:14:18 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# cbr2cbz
+
+Little script that converts a file in .cbr format to .cbz format.
+
+Needs zip and unrar.
+
+Use:
+```sh
+cbr2cbz.sh path/to/file.cbr
+```
+
+Creates a file called `path/to/file.cbz`.
+
+
---
+++ cbr2cbz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "origin: $1"
+cbr_file=$1
+
+IFS="
+"
+
+tempdir=$(mktemp -d)
+echo $tempdir
+
+filename=$(basename "$cbr_file")
+filename_without_extension="${filename%.*}"
+cp "$cbr_file" $tempdir/"${filename}".rar
+mkdir $tempdir/"${filename_without_extension}"
+unrar x $tempdir/"${filename}".rar $tempdir/"${filename_without_extension}"
+
+zip -r $tempdir/"${filename_without_extension}".zip $tempdir/"${filename_without_extension}"/*
+
+cp $tempdir/"${filename_without_extension}".zip "${cbr_file%.*}".cbz
+
+rm -r $tempdir
